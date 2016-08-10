83d546585b0eb827ba1459aa2429639d046740c3
dsolovay <dan.solovay@velir.com>
2016-08-10 14:00:32 -0400

    Add project files.

---
+++ ItemCreation/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemCreation
+{
+  public class ItemCreationTests
+  {
+    fact
+  }
+}
