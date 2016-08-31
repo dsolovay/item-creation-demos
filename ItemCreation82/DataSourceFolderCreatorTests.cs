b4771a38e67d56467c54179b84e850d7d0e55d56
dsolovay <dan.solovay@velir.com>
2016-08-31 11:31:44 -0400

    green

--- ItemCreation82/DataSourceFolderCreatorTests.cs
+++ ItemCreation82/DataSourceFolderCreatorTests.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.Exceptions;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using Sitecore.Pipelines.GetRenderingDatasource;
+using Xunit;
+using Version = Sitecore.Data.Version;
 
 namespace ItemCreation82
 {
-	class DataSourceFolderCreatorTests
+	public class DataSourceFolderCreatorTests
 	{
+		[Fact]
+		public void Process_ItemFolderMissing_CreatesIt()
+		{
+			var sut = new DataSourceCreatorProcessor();
+			var db = Substitute.For<Database>();
+			Item renderingItem = MakeItem(db, ID.NewID, "some rendering item");
+			ItemPath itemPath = Substitute.For<ItemPath>(renderingItem);
+			renderingItem.Paths.Returns(itemPath);
+			renderingItem.Paths.FullPath.Returns("/some/path");
+		 
+			GetRenderingDatasourceArgs args = new GetRenderingDatasourceArgs(renderingItem, db);
+
+			sut.Process(args);
+
+			renderingItem.Received().Add("Items", new TemplateID(TemplateIDs.Folder));
+
+		}
+
+		private Item MakeItem(Database db, ID id, string name)
+		{
+			ItemData data = new ItemData(new ItemDefinition(id, name, ID.Null, ID.Null), Language.Current, Version.First,
+				new FieldList()); 
+			return Substitute.For<Item>(id, data, db);
+		}
+	}
+
+	public class DataSourceCreatorProcessor
+	{
+		public void Process(GetRenderingDatasourceArgs args)
+		{
+			args.RenderingItem.Add("Items", new TemplateID(TemplateIDs.Folder));
+		}
 	}
 }
+
