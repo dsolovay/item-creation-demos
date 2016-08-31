683ca7bcd583fe1246617e2d415d92da90b0ae7c
dsolovay <dan.solovay@velir.com>
2016-08-31 14:30:59 -0400

    Add test for folder already present

--- ItemCreation82/DataSourceFolderCreatorTests.cs
+++ ItemCreation82/DataSourceFolderCreatorTests.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using NSubstitute;
 using NSubstitute.Exceptions;
 using Sitecore;
+using Sitecore.Collections;
 using Sitecore.Data;
+using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 using Sitecore.Globalization;
 using Sitecore.Pipelines.GetRenderingDatasource;
@@ -22,10 +25,8 @@ namespace ItemCreation82
 		{
 			var sut = new DataSourceCreatorProcessor();
 			var db = Substitute.For<Database>();
-			Item renderingItem = MakeItem(db, ID.NewID, "some rendering item", "/some/path");
-
-		 
-			GetRenderingDatasourceArgs args = new GetRenderingDatasourceArgs(renderingItem, db);
+			Item renderingItem = MakeItem(db, ID.NewID, "some rendering item", "/some/path", ID.Null);
+			var args = new GetRenderingDatasourceArgs(renderingItem, db);
 
 			sut.Process(args);
 
@@ -33,11 +34,31 @@ namespace ItemCreation82
 
 		}
 
-		private Item MakeItem(Database db, ID id, string name, string path)
+		[Fact]
+		public void Process_ItemFolderPresent_DoesNotCreateNewOne()
 		{
-			ItemData data = new ItemData(new ItemDefinition(id, name, ID.Null, ID.Null), Language.Current, Version.First,
+			var sut = new DataSourceCreatorProcessor();
+			var db = Substitute.For<Database>();
+			Item renderingItem = MakeItem(db, ID.NewID, "some rendering item", "/some/path", ID.NewID);
+			Item childItem = MakeItem(db, ID.NewID, "Items", "/some/path", TemplateIDs.Folder);
+			renderingItem.GetChildren().Returns(new ChildList(renderingItem, new ItemList {childItem}));
+			renderingItem.GetChildren().Count.Should().Be(1);
+			renderingItem.GetChildren().First().Name.Should().Be("Items");
+
+			var args = new GetRenderingDatasourceArgs(renderingItem, db);
+
+			sut.Process(args);
+
+			renderingItem.DidNotReceive().Add("Items", new TemplateID(TemplateIDs.Folder));
+
+		}
+
+		private Item MakeItem(Database db, ID id, string name, string path, ID templateId)
+		{
+			ItemData data = new ItemData(new ItemDefinition(id, name, templateId, ID.Null), Language.Current, Version.First,
 				new FieldList()); 
 			var item = Substitute.For<Item>(id, data, db);
+			item.Name.Returns(name);
 			item.Paths.Returns(Substitute.For<ItemPath>(item));
 			item.Paths.FullPath.Returns(path);
 			return item;
